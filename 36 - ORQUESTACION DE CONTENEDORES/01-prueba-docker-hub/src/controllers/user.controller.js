84bcd249a554e1029754d93af9e02e6ecb7b73e8
MauriDiPietro <dipietro.jm@gmail.com>
2024-08-13 21:03:08 -0300

    clase36

---
+++ 36 - ORQUESTACION DE CONTENEDORES/01-prueba-docker-hub/src/controllers/user.controller.js
@@ -0,0 +1,22 @@
+import * as userService from "../services/user.service.js";
+
+export const createUser = async (req, res) => {
+  const { cant } = req.query;
+  try {
+    const response = await userService.createUsersMock(cant);
+    res.status(200).json({ users: response });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getUsers = async (req, res) => {
+  try {
+    const response = await userService.getUsers();
+    res.status(200).json({ users: response });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
