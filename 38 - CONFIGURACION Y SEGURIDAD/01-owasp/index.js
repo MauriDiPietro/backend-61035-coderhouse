b64319880aa769c6d309a1af78b65705e774d0e8
MauriDiPietro <dipietro.jm@gmail.com>
2024-08-20 20:37:04 -0300

    clase38

--- 38 - CONFIGURACION Y SEGURIDAD/01-owasp/index.js
+++ 38 - CONFIGURACION Y SEGURIDAD/01-owasp/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import helmet from 'helmet'
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet())
 
 app.get('/', (req, res) => res.send('Welcome'));
 
