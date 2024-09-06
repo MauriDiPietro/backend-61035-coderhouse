416c4f3f26532fa1de824efe3f9e46eef20e6e5c
MauriDiPietro <dipietro.jm@gmail.com>
2024-09-06 07:05:10 -0300

    type generateToken

--- 44 - NEST 2/api-users/src/user/user.service.ts
+++ 44 - NEST 2/api-users/src/user/user.service.ts
@@ -34,7 +34,7 @@ export class UserService {
     }
   }
 
-  generateToken(user: UserDocument) {
+  generateToken(user: UserDocument): string {
     const payload = {
       first_name: user.first_name,
       last_name: user.last_name,
